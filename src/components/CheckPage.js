672ef2f26708008cf3f4cf6371c1958c188b38e1
spectrumsun <spectrumsun@hotmail.com>
2019-10-26 19:51:05 +0100

    add check up page

--- src/components/CheckPage.js
+++ src/components/CheckPage.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Image from '../Image/Group.svg';
 
 const CheckPage = () => {
   return (
-    <>
-    <h3>This na check page</h3>
-    </>
+    <div class="jumbotron check-page">
+    <img src={Image} className='black_search_icon' alt='Search Icon' />
+    <h3>LOL</h3>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import './App.css';
 
 import Firebase, { FirebaseContext } from './firebase';
 
