67451abfc3046d1585d83d559111906a03a30b8c
victor <victorugwueze@gmail.com>
2019-10-26 17:46:38 +0100

    add route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import HomePage from './components/HomePage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={HomePage} />
+        {/* <Route path='/onboarding' exact component={Onboarding} /> */}
+        {/* <Route path='/authenticate' exact component={Authenticate} /> */}
+      </Switch>
+    </BrowserRouter>
   );
 }
 
