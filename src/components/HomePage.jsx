3721eb81e0b8244f19d4a2fa1bbaa4c9865f5525
victor <victorugwueze@gmail.com>
2019-10-27 12:01:04 +0100

    finish report

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -41,10 +41,14 @@ class HomePage extends Component {
 
   onSubmit = async (e) => {
     e.preventDefault()
+    this.setState(state => ({
+      isLoading: true
+    }));
     if(!this.state.file){
-      this.finishReport()
+      this.finishReport();
+      return;
     }
-    // this.setState({ triggerUpload: true })
+    this.setState({ triggerUpload: true })
   }
 
   searchVehicle = async (e) => {
@@ -55,13 +59,14 @@ class HomePage extends Component {
   }
 
   finishReport = async url => {
-    // this.setState({ triggerUpload: false })
-    // await this.props.firebase.sendReport({ ...this.state.vehicle, imageUrl: url });
-    this.props.firebase.app.functions()
-    .httpsCallable('sendSMS')({
-      message: "This vehicle has been reported lost please be on the look out%0aCAR: HONDA%0aVIN: 86528HTE88%0aPLATE NUMBER: EKY769JK"
-      // message: 'A car with this number has been reported stolen'
-    })
+    this.setState({ triggerUpload: false })
+    await this.props.firebase.sendReport({ ...this.state.vehicle, imageUrl: url });
+    // this.props.firebase.app.functions()
+    // .httpsCallable('sendSMS')({
+    //   message: "This vehicle has been reported lost please be on the look out\nCAR: HONDA\nVIN: 86528HTE88\nPLATE NUMBER: EKY769JK"
+    //   // message: 'A car with this number has been reported stolen'
+    // });
+    this.setState({ isLoading: false })
   }
 
   handleImageUpload = (file) => {
@@ -70,84 +75,80 @@ class HomePage extends Component {
     });
   };
   render() {
-    const {displayImage} = this.state
+    const { displayImage, triggerUpload, isLoading } = this.state
     return (
       <div className="homepage">
-     
-<div className="content">
-<div className="header-title">
-<h1>Report your stolen car</h1>
-      </div>
-  <form className="homepage-form" onSubmit={this.onSubmit}>
- 
-  <div className="row row-1">
-    <div className="col">
-      <input type="text" className="form-control form-control-col" name="plate_number" onChange={this.handleInputChange} placeholder="Plate number"/>
-    </div>
-    <div className="col">
-      <input type="text" className="form-control form-control-col" name="VIN"placeholder="Enter VIN" onChange={this.handleInputChange}/>
-    </div>
-    <div className="col">
-      <input type="text" className="form-control form-control-col" name="license"placeholder="Drivers License" onChange={this.handleInputChange}/>
-    </div>
-  </div>
-  <div className="row row-2">
-    <div className="col row-2-col-1">
-      <input type="text" className="form-control form-control-col-1 form-control-col" name="location"placeholder="Enter last seen location" onChange={this.handleInputChange}/>
-    </div>
-    <div className="col row-2-col-2">
-    <DateTimePicker
-      dropDown
-      className="form-control form-control-col"
-      placeholder="Time of theft"
-      value={this.state.datetime}
-      onChange={value => this.setState({
-        datetime: new Date(value)
-      })}
-      />
-    </div>
-    
-  </div>
-  
-  <div className="row">
-  <div className="col">
-  {displayImage &&
-      <img className="upload-image" src={displayImage} alt="al" />
-      }
-    </div>
-  
-  </div>
-  <div className="row row-3">
-    
-    <div className="col col-btn-row">
-    <button type="submit" className="btn btn-primary btn-row" >SUBMIT REPORT <div class="spinner-border" role="status">
-  <span class="sr-only">Loading...</span> </div></button>
-    </div>
+        <div className="content">
+          <div className="header-title">
+            <h1>Report your stolen car</h1>
+          </div>
+        <form className="homepage-form" onSubmit={this.onSubmit}>
+          <div className="row row-1">
+            <div className="col">
+              <input type="text" className="form-control form-control-col" name="plate_number" onChange={this.handleInputChange} placeholder="Plate number"/>
+            </div>
+            <div className="col">
+              <input type="text" className="form-control form-control-col" name="VIN"placeholder="Enter VIN" onChange={this.handleInputChange}/>
+            </div>
+            <div className="col">
+              <input type="text" className="form-control form-control-col" name="license"placeholder="Drivers License" onChange={this.handleInputChange}/>
+            </div>
+          </div>
+          <div className="row row-2">
+            <div className="col row-2-col-1">
+              <input type="text" className="form-control form-control-col-1 form-control-col" name="location"placeholder="Enter last seen location" onChange={this.handleInputChange}/>
+            </div>
+            <div className="col row-2-col-2">
+            <DateTimePicker
+              dropDown
+              className="form-control form-control-col"
+              placeholder="Time of theft"
+              value={this.state.datetime}
+              onChange={value => this.setState({
+                datetime: new Date(value)
+              })}
+              />
+            </div>
 
-    <div className="col col-btn-row">
-    <div className="upload-btn-wrapper">
-  {/* <button className="btn-btn">Upload car images</button> */}
-    <ImageUploader
-      triggerUpload={triggerUpload}
-      onChange={(file) => {
-        this.handleImageUpload(file);
-        this.setState(state => ({
-          ...state,
-          file,
-          imageUrl: ''
-        }))
-      }}
-      onUpload={(url) => {
-        this.finishReport(url)
-      }}
-    />
-  {/* <input type="file" name="myfile" accept="image/*" multiple onChange={}/> */}
-</div>
-    </div> 
-  </div>
-</form>
-</div>
-</div>
+          </div>
+      <div className="row">
+      <div className="col">
+      {displayImage &&
+          <img className="upload-image" src={displayImage} alt="al" />
+          }
+        </div>
+      </div>
+      <div className="row row-3">
+        <div className="col col-btn-row">
+        <button type="submit" className="btn btn-primary btn-row" >
+          SUBMIT REPORT 
+          {isLoading &&  <div class="spinner-border" role="status">
+          <span class="sr-only">Loading...</span> </div> }
+        </button>
+        </div>
+
+        <div className="col col-btn-row">
+        <div className="upload-btn-wrapper">
+        <ImageUploader
+          triggerUpload={triggerUpload}
+          onChange={(file) => {
+            this.handleImageUpload(file);
+            this.setState(state => ({
+              ...state,
+              file,
+              imageUrl: ''
+            }))
+          }}
+          onUpload={(url) => {
+            this.finishReport(url)
+          }}
+        />
+    </div>
+        </div> 
+      </div>
+    </form>
+    </div>
+    </div>
     )
   }
 }
