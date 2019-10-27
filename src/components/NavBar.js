9ce17c81a8040c99ef7d113213e056e77862dfd7
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2019-10-27 12:52:26 +0100

    search bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,46 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const HomePage = () => {
-    return (
-      <>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-              <Link
-                id="logout"
-                className="nav-link"
-                to="/home-page"
-                >
-                  HomePage
-                </Link>
-              </li>
-       
-              <li className="nav-item">
+  const [search, setSearch] = useState('');
 
-              <Link
-                id="logout"
-                className="nav-link"
-                to="/check-page"
-                >
+  const searchCar = () => {};
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <Link id="logout" to="/home-page">
+                <a className="nav-link" href="#">
+                  Home <span className="sr-only">(current)</span>
+                </a>
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link id="logout" to="/check-page">
+                <a className="nav-link" href="#">
                   CheckPage
-                </Link>
-
-
-              </li>
-              </ul>
-              <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </div>
-  </nav>
-  </>
-  )
-}
+                </a>
+              </Link>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0" onSubmit={searchCar}>
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              onChange={setSearch}
+              value={search}
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default HomePage;
