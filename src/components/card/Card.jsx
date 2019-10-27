e20e60ec10b595e9a02154a171cafff9051a31ce
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2019-10-27 12:11:49 +0100

    card component

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Card = ({ title, description, image }) => {
+  return (
+    <div className="card" style="width: 18rem;">
+      <img src={image} className="card-img-top" alt="title" />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <Link to="/" className="btn btn-primary">
+          View Report
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
